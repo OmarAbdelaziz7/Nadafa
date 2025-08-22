4829525e5b9838afa25ac50e34ed8e3813bd4f34
Motarek2003 <Mohamed.TarekAfifi@gmail.com>
2025-08-22 18:52:25 +0300

    Intial Commit with Architecture Setup

---
+++ NetCoreAPICleanArchTemplate/Application/ApplicationServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Application
+{
+    public static class ApplicationServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //services.AddScoped<ISP_Call, SP_Call>();
+            //services.AddScoped<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
---
+++ NetCoreAPICleanArchTemplate/Application/Contracts/IUsersService.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Domain.Entities;
+using Domain.Results;
+
+namespace Application.Contracts
+{
+    public interface IUsersService
+    {
+        public Response<List<User>> getAll();
+        public Response<User> get();
+        public Response<User> add();
+        public Response<User> update();
+        public Response<string> delete();
+    }
+}
