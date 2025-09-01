914c823ac340e48a085dd7870818cf6d1f6ac081
OmarAbdelaziz7 <ozizo2468@gmail.com>
2025-09-01 10:10:08 +0300

    added House Products

---
+++ NadafaFrontEnd/src/app/features/house-products/components/house-products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-house-products',
+  imports: [],
+  templateUrl: './house-products.html',
+  styleUrl: './house-products.css'
+})
+export class HouseProducts {
+
+}
